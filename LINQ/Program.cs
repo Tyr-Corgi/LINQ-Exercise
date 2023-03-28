17e90d007e88d58651fef8063a1530f749e866d7
Tyler Gaisford <tyler.gaisford@gmail.com>
2023-03-28 10:46:26 -0600

    Completed

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace LINQ
+﻿using System.Globalization;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videoGames = new List<string>()
+           { "Zelda", "Fortnite", "Borderlands", "Metal Gear Solid", "Ratchet and Clank", "Zak", "Fallout", "Marathon", "Minecraft" };
+
+            var videoGameOrder = videoGames.OrderBy(name => name.Length);
+
+            foreach ( var videoGameList in videoGameOrder) 
+            {
+                Console.WriteLine(videoGameList);
+            }
         }
     }
 }
